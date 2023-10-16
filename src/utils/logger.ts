0c2fdeacc88a5422b1d5a3177f86ea03bc2cd68a
Rajneshwar Singh <rajneshwarthakur@gmail.com>
2023-10-16 08:14:19 +0530

    Update logger file

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,105 +1,78 @@
 import { v4 as uuidv4 } from 'uuid';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const namespace = require('continuation-local-storage').getNamespace('logger');
-import { createLogger, transports, format } from 'winston';
-import 'winston-daily-rotate-file'; // Add DailyRotateFile to Winston transports
+import { getNamespace } from 'continuation-local-storage';
+import { createLogger, format, transports, Logger } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+import appRoot from 'app-root-path';
+import path from 'path';
 
-const logsPath = `${process.cwd()}/logger/logs/`;
+const namespace = getNamespace('logger');
+const logsPath = path.join(appRoot.path, 'lib/logger/logs/');
 
 const fileFormat = format.combine(
   format.timestamp(),
   format.align(),
-  // eslint-disable-next-line consistent-return
-  format.printf((info) => {
-    let log = '';
+  format.printf((info: any) => {
     try {
-      const logId = namespace && namespace.get('logId') ? namespace.get('logId') : uuidv4();
+      const logId = namespace?.get('logId') || uuidv4();
       const { timestamp, level, message, ...args } = info;
       const ts = timestamp.slice(0, 19).replace('T', ' ');
-      log = `${ts} - ${logId} - ${level}: ${message ? message.trim() : ''} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
-      return log;
+      return `${ts} - ${logId} - ${level}: ${message?.trim() || ''} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
     } catch (error) {
-      console.log('Error @ fileFormat @ logger ', error);
+      console.error('Error @ fileFormat @ logger', error);
+      return '';
     }
   }),
 );
-//Create the logger
-const info = createLogger({
-  level: 'info',
-  format: fileFormat,
-  transports: [
-    new transports.DailyRotateFile({
-      level: 'info',
-      filename: `${logsPath}/info-%DATE%.log`,
-      handleExceptions: true,
-      json: true,
-      maxSize: 100000, // 5MB
-    }),
-    new transports.Console({
-      level: 'error',
-      format: format.combine(format.colorize(), format.simple()),
-    }),
-  ],
-});
 
-const error = createLogger({
-  level: 'error',
-  format: fileFormat,
-  transports: [
-    new transports.DailyRotateFile({
-      level: 'error',
-      filename: `${logsPath}/error-%DATE%.log`,
-      handleExceptions: true,
-      json: true,
-      maxSize: 5242880, // 5MB
-    }),
-    new transports.Console({
-      level: 'error',
-      format: format.combine(format.colorize(), format.simple()),
-    }),
-  ],
-});
+const createCustomLogger = (logLevel: string, fileName: string): Logger => {
+  return createLogger({
+    level: logLevel,
+    format: fileFormat,
+    transports: [
+      new DailyRotateFile({
+        level: logLevel,
+        filename: `${logsPath}/${fileName}-%DATE%.log`,
+        handleExceptions: true,
+        json: true,
+        // maxsize: 5242880, // 5MB
+        // colorize: false,
+      }),
+    ],
+  });
+};
+
+const infoLogger = createCustomLogger('info', 'info');
+const errorLogger = createCustomLogger('error', 'error');
 
 const consoleFormat = format.combine(
   format.colorize(),
   format.timestamp(),
   format.align(),
-  // eslint-disable-next-line consistent-return
   format.printf((info: any) => {
-    let log = '';
     try {
-      const logId = namespace && namespace.get('logId') ? namespace.get('logId') : uuidv4();
+      const logId = namespace?.get('logId') || uuidv4();
       const { timestamp, level, message, ...args } = info;
       const ts = timestamp.slice(0, 19).replace('T', ' ');
-      log = `${ts} - ${logId} - ${level}: ${message ? message.trim() : ''} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
-      return log;
+      return `${ts} - ${logId} - ${level}: ${message?.trim() || ''} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
     } catch (error) {
-      console.log('Error @ fileFormat @ logger ', error);
+      console.error('Error @ consoleFormat @ logger', error);
+      return '';
     }
   }),
 );
 
 if (process.env.NODE_ENV !== 'production') {
-  info.add(
-    new transports.Console({
-      format: consoleFormat,
-    }),
-  );
-  error.add(
-    new transports.Console({
-      format: consoleFormat,
-    }),
-  );
+  infoLogger.add(new transports.Console({ format: consoleFormat }));
+  errorLogger.add(new transports.Console({ format: consoleFormat }));
 }
 
 const logger = {
-  info: async (msg, ...args) => {
-    info.info(msg, ...args);
+  info: async (msg: string, ...args: any[]) => {
+    infoLogger.info(msg, ...args);
   },
-  error: async (msg: any, ...args: any) => {
-    console.log('msg', msg);
-    console.log('args', args);
-    error.error(msg, ...args);
+  error: async (msg: string, ...args: any[]) => {
+    errorLogger.error(msg, ...args);
   },
 };
+
 export default logger;
